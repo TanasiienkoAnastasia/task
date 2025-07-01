9ab17be01cdc41c65c0746491c51da3fe96b7d4b
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2025-07-01 16:21:55 +0300

    add CsvMapper and CsvSchema

--- src/main/java/com/example/task1/HelloApplication.java
+++ src/main/java/com/example/task1/HelloApplication.java
@@ -3,7 +3,12 @@ import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
+import java.util.List;
+import java.util.Map;
+
 
 public class HelloApplication  {
-
+    CsvMapper csvMapper = new CsvMapper();
+    CsvSchema schema = CsvSchema.emptySchema().withHeader();
+    List<Map<String, String>> data;
 }
