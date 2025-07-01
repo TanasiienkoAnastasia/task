8fb00de09e65b91d76d4a8852d034b84a48b20c3
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2025-07-01 16:19:09 +0300

    add files

---
+++ src/main/java/com/example/task1/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.task1;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+
+public class HelloApplication  {
+
+}
