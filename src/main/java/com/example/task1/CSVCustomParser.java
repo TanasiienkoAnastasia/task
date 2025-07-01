26c993ab3e025b6084b3c88d92c06f347946030f
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2025-07-01 17:21:13 +0300

    find arithmetic mean of grades

--- src/main/java/com/example/task1/CSVCustomParser.java
+++ src/main/java/com/example/task1/CSVCustomParser.java
@@ -2,6 +2,8 @@ package com.example.task1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CSVCustomParser {
@@ -11,17 +13,28 @@ public class CSVCustomParser {
             String line;
             while((line = br.readLine()) != null) {
                 String[] values = parseLine(line);
+                ArrayList<Integer> avMean = new ArrayList<Integer>();
                 for (String value : values) {
                     System.out.print(value + " ");
+                     if(value.matches(".*\\d.*")){
+                         int number = Integer.parseInt(value);
+                         avMean.add(number) ;
+                     }
+                    System.out.println();
                 }
-                System.out.println();
+                System.out.println(avMean.stream()
+                        .mapToDouble(d -> d)
+                        .average()
+                        .orElse(0.0));
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
     }
+
     private static String[] parseLine(String line) {
         // Handle commas, quotes, etc.
         return line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
     }
+
 }
